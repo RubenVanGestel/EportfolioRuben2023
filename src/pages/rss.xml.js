9fae458a3d9483800c26626186af7a05f7caa7c3
Ruben Van Gestel <Ruben.Van.Gestel@wilms.be>
2023-06-09 18:53:46 +0200

    bugfixing

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ import { getCollection } from "astro:content";
 export async function get(context) {
   const blog = await getCollection("blog");
   return rss({
-    title: EPortfolio_Ruben,
+    title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
     items: blog.map((post) => ({
